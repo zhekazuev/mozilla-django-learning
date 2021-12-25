c38d1891540acaf1a8f42ec48488efff5fd1d73f
Eugene Zuev <zhekazuev@gmail.com>
2021-12-25 03:42:49 +0100

    Add language model

--- src/locallibrary/catalog/models.py
+++ src/locallibrary/catalog/models.py
@@ -18,6 +18,21 @@ class Genre(models.Model):
         return self.name
 
 
+class Language(models.Model):
+    """
+    Model representing a Language (e.g. Russian, English etc.)
+    """
+    name = models.CharField(max_length=200, 
+        help_text="Enter a book language (e.g. Russian, English etc.)")
+
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+
 class Book(models.Model):
     """
     Model representing a book (but not a specific copy of a book).
@@ -32,6 +47,7 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
 
     def __str__(self):
