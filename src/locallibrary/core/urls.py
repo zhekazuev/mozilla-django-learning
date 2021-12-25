79b0a26accd422b48a735d768b0c9a47a308d3b7
Eugene Zuev <zhekazuev@gmail.com>
2021-12-25 02:04:37 +0100

    add static to urlpatterns

--- src/locallibrary/core/urls.py
+++ src/locallibrary/core/urls.py
@@ -13,13 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.views.generic import RedirectView
-from django.conf.urls import include
+
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf.urls import include
+from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
+
+# for develop
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
