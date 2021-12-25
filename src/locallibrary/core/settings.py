54cdbfff6c69cb8b09f80919fc706739d12d59d7
Eugene Zuev <zhekazuev@gmail.com>
2021-12-25 02:01:16 +0100

    add timezone from env and default

--- src/locallibrary/core/settings.py
+++ src/locallibrary/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv("DJANGO_TIME_ZONE", "Europe/Warsaw")
 
 USE_I18N = True
 
